0e1f4e25d7e8554cd81098c4d05f9d62ef8a4703
Amit Swisa <amitswisa5@gmail.com>
2023-01-27 18:03:32 +0200

    Delete CosmicRaceWebServer directory

--- CosmicRaceWebServer/config/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  jwtSecretKey:
-    "BxnMBhcCLLUp6S5ApcUPn3glxKK9rnl40Kdn4SS4HgidpPUCMt/Xhga1eZnkVcMv",
-  dbHost: "cosmicrace-db.mysql.database.azure.com",
-  dbuser: "cosmicracedb",
-  dbpassword: "RhPV07%6yIo9",
-  dbdatabase: "cosmicrace_db",
-};
